9ded9caedd31ad236b2f77bb270844c5e7fbcc09
James Skemp <git@jamesrskemp.com>
2025-06-27 08:56:55 -0500

    Update history display to centralize yyyy-mm-dd string pulling
    Did generate a bug with the table view of items and sprint start dates.

--- src/Hub/IterationHistoryDisplay.tsx
+++ src/Hub/IterationHistoryDisplay.tsx
@@ -305,7 +305,7 @@ export class IterationHistoryDisplay extends React.Component<IterationHistoryDis
 		let addIterationEndRowAfter = false;
 		const preIterationChanges = { changes: 0, storyPoints: 0 };
 		const postIterationChanges = { changes: 0, storyPoints: 0 };
-		const canDisplayEndSprintRow = this.props.iteration && (new Date()).toISOString() > iterationFinishDate.toISOString();
+		const canDisplayEndSprintRow = this.props.iteration && this.getDateStringFormat(new Date(), 'YYYYMMDD') > this.getDateStringFormat(iterationFinishDate, 'YYYYMMDD');
 
 		return (
 			<Card className="iteration-history-display"
@@ -346,18 +346,18 @@ export class IterationHistoryDisplay extends React.Component<IterationHistoryDis
 								addedIterationEndRow = true;
 							}
 
-							if (!addedIterationStartRow && wi.changedDateFull.toISOString().split("T")[0] < iterationStartDate.toISOString().split("T")[0]) {
+							if (!addedIterationStartRow && this.getDateStringFormat(wi.changedDateFull, 'YYYYMMDD') < this.getDateStringFormat(iterationStartDate, 'YYYYMMDD')) {
 								preIterationChanges.changes++;
 								preIterationChanges.storyPoints = wi.totalStoryPoints;
 								return null;
 							}
 
-							if (!addedIterationStartRow && wi.changedDateFull.toISOString().split("T")[0] >= iterationStartDate.toISOString().split("T")[0]) {
+							if (!addedIterationStartRow && this.getDateStringFormat(wi.changedDateFull, 'YYYYMMDD') >= this.getDateStringFormat(iterationStartDate, 'YYYYMMDD')) {
 								addIterationStartRow = true
 							}
 							if (!addedIterationEndRow && i === storyPointChanges.length - 1) {
 								addIterationEndRowAfter = true;
-								if (wi.changedDateFull.toISOString().split("T")[0] > iterationFinishDate.toISOString().split("T")[0]) {
+								if (this.getDateStringFormat(wi.changedDateFull, 'YYYYMMDD') > this.getDateStringFormat(iterationFinishDate, 'YYYYMMDD')) {
 									postIterationChanges.changes++;
 									postIterationChanges.storyPoints = wi.totalStoryPoints;
 									if (canDisplayEndSprintRow) {
@@ -368,7 +368,7 @@ export class IterationHistoryDisplay extends React.Component<IterationHistoryDis
 										return null;
 									}
 								}
-							} else if (!addedIterationEndRow && wi.changedDateFull.toISOString().split("T")[0] > iterationFinishDate.toISOString().split("T")[0]) {
+							} else if (!addedIterationEndRow && this.getDateStringFormat(wi.changedDateFull, 'YYYYMMDD') > this.getDateStringFormat(iterationFinishDate, 'YYYYMMDD')) {
 								postIterationChanges.changes++;
 								postIterationChanges.storyPoints = wi.totalStoryPoints;
 								return null;
@@ -419,11 +419,11 @@ export class IterationHistoryDisplay extends React.Component<IterationHistoryDis
 								addedIterationEndRow = true;
 							}
 
-							if (!addedIterationStartRow && wi.changedDateFull.toISOString().split("T")[0] >= iterationStartDate.toISOString().split("T")[0]) {
+							if (!addedIterationStartRow && this.getDateStringFormat(wi.changedDateFull, 'YYYYMMDD') >= this.getDateStringFormat(iterationStartDate, 'YYYYMMDD')) {
 								addIterationStartRow = true
 							}
 
-							if (!addedIterationEndRow && wi.changedDateFull.toISOString().split("T")[0] > iterationFinishDate.toISOString().split("T")[0]) {
+							if (!addedIterationEndRow && this.getDateStringFormat(wi.changedDateFull, 'YYYYMMDD') > this.getDateStringFormat(iterationFinishDate, 'YYYYMMDD')) {
 								addIterationEndRowBefore = true;
 							} else if (!addedIterationEndRow && i === storyPointChanges.length - 1) {
 								addIterationEndRowAfter = true;
@@ -485,7 +485,7 @@ export class IterationHistoryDisplay extends React.Component<IterationHistoryDis
 		const currentDate = new Date(startDate);
 		const endDateOnly = new Date(endDate).toISOString().split("T")[0];
 
-		while (currentDate.toISOString().split("T")[0] <= endDateOnly) {
+		while (this.getDateStringFormat(currentDate, 'YYYYMMDD') <= endDateOnly) {
 			dateArray.push(new Date(currentDate).toLocaleDateString());
 			currentDate.setUTCDate(currentDate.getUTCDate() + steps);
 		}
@@ -541,5 +541,13 @@ export class IterationHistoryDisplay extends React.Component<IterationHistoryDis
 		});
 
 		return storyPoints;
+	}
+
+	private getDateStringFormat(date: Date, format: string): string {
+		if (format === 'YYYYMMDD') {
+			return [date.getFullYear(), ('0' + (date.getMonth() + 1)).slice(-2), ('0' + date.getDate()).slice(-2)].join('-');
+		}
+		// Same format for now.
+		return [date.getFullYear(), ('0' + (date.getMonth() + 1)).slice(-2), ('0' + date.getDate()).slice(-2)].join('-');
 	}
 }
